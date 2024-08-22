f1aadad4b7c21123f4bf0e2424cbf39bb6b4e77e
Pradikshan <pradikshan234@gmail.com>
2024-08-22 12:30:22 +0530

    Initial commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Sparkles } from "../components/Sparkles";
+import { VortexDemo } from "../components/Vortex";
+
+const Home = () => {
+  return (
+    <>
+      <Sparkles />
+      {/* <VortexDemo /> */}
+    </>
+  );
+};
+
+export default Home;
