9c3237638bae51bd0a70e504c2d197083b4bca59
Pradikshan <pradikshan234@gmail.com>
2024-08-22 15:23:35 +0530

    Added TypeWriterEffect

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Sparkles } from "../components/Sparkles";
 import { VortexDemo } from "../components/Vortex";
+import { TypewriterTextEffect } from "../components/TypeWriterEffect";
+import { WavyBackgroundEffect } from "../components/WavyBackgroundEffect";
+import { GoogleGeminiHeroEffect } from "../components/GoogleGeminiHeroEffect";
 
 const Home = () => {
   return (
     <>
       <Sparkles />
+      {/* <WavyBackgroundEffect /> */}
+      {/* <GoogleGeminiHeroEffect /> */}
       {/* <VortexDemo /> */}
+      <TypewriterTextEffect />
     </>
   );
 };
