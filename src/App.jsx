9ffeda228ebf129a80b4c2d299f2df37de92219d
Pradikshan <pradikshan234@gmail.com>
2024-08-22 12:29:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import FeaturedCard from "./components/FeaturedCard";
+import Home from "./views/Home";
+
+function App() {
+  return (
+    <>
+      {/* <div className="bg-red-500">
+        <p>HEllo world</p>
+      </div>
+      <FeaturedCard />
+      <p>Hello men</p> */}
+      <Home />
+    </>
+  );
+}
+
+export default App;
