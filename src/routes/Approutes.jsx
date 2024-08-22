978737be76fd9b17b86054f0d23b9eb48d6e7929
Pradikshan <pradikshan234@gmail.com>
2024-08-22 20:11:16 +0530

    Added route path to views

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,10 +1,18 @@
-import Home from "../views/Home";
 import { Routes, Route } from "react-router-dom";
+import Home from "../views/Home";
+import Features from "../views/Features";
+import Pricing from "../views/Pricing";
+import About from "../views/About";
+import Support from "../views/Support";
 
 export const Approutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/features" element={<Features />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/support" element={<Support />} />
     </Routes>
   );
 };
