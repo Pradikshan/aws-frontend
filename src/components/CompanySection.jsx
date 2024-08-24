0b904d5831936b5cd2051778ce971382ced626cf
Pradikshan <pradikshan234@gmail.com>
2024-08-24 11:49:01 +0530

    Created layout

--- src/components/CompanySection.jsx
+++ src/components/CompanySection.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const CompanySection = () => {
-  return <div>CompanySection</div>;
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-white text-xl">Our Partners</p>
+        <div className="flex flex-row justify-between"></div>
+      </div>
+    </div>
+  );
 };
 
 export default CompanySection;
