3cf5929102e629fad5715421f6d4636db8057fe3
Pradikshan <pradikshan234@gmail.com>
2024-08-24 21:05:11 +0530

    Added component

--- src/views/Support.jsx
+++ src/views/Support.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FAQ from "../components/FAQ";
+import Contactus from "../components/Contactus";
 
 const Support = () => {
   return (
     <div>
       <FAQ />
+      <Contactus />
     </div>
   );
 };
