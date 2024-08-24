78162e7ef78af8ac3dc4408e799d8ff004f80157
Pradikshan <pradikshan234@gmail.com>
2024-08-24 12:09:21 +0530

    Changed section heading style

--- src/components/FeaturesOverview.jsx
+++ src/components/FeaturesOverview.jsx
@@ -6,9 +6,7 @@ import FeaturedCard from "./FeaturedCard";
 const FeaturesOverview = () => {
   return (
     <div className="mx-6">
-      <h1 className="text-5xl text-white font-semibold text-center">
-        Features Overview
-      </h1>
+      <h1 className="section-heading text-center">Features Overview</h1>
       <div className="grid grid-cols-3 gap-8 my-7">
         {/* <FeaturesBox />
         <FeaturesBox />
