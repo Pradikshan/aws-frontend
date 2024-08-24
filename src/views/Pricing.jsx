5b32892f94ef254e5bfcece9495884d32662498c
Pradikshan <pradikshan234@gmail.com>
2024-08-24 16:05:19 +0530

    Cretaed layout and imported pricingc ard component

--- src/views/Pricing.jsx
+++ src/views/Pricing.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import PricingCard from "../components/PricingCard";
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <div className="mt-36">
+      <p className="section-heading text-center">
+        We have a pricing plan that fits every need
+      </p>
+      <div className="grid grid-cols-4 gap-4 mx-8">
+        <PricingCard />
+      </div>
+    </div>
+  );
 };
 
 export default Pricing;
