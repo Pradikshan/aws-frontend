304e75de125aba42e9e4b550e1730b89de14777b
Pradikshan <pradikshan234@gmail.com>
2024-08-24 12:02:36 +0530

    Added images

--- src/components/CompanySection.jsx
+++ src/components/CompanySection.jsx
@@ -3,10 +3,26 @@ import React from "react";
 const CompanySection = () => {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center my-20">
-        <p className="text-white text-5xl font-bold my-5">Our Partners</p>
+      <div className="flex flex-col justify-center items-center mt-36">
+        <p className="text-white text-5xl font-semibold my-16">
+          Our Music Partners
+        </p>
         <div className="flex flex-row justify-between">
-          <img src="/public/images/warner.png" alt="company-logo" width={340} />
+          <img
+            src="/public/images/universal.jpg"
+            alt="company-logo"
+            className="w-80 mx-8"
+          />
+          <img
+            src="/public/images/warner.png"
+            alt="company-logo"
+            className="w-80 h-40 mx-8"
+          />
+          <img
+            src="/public/images/sony.jpg"
+            alt="company-logo"
+            className="w-60 h-40 mx-8"
+          />
         </div>
       </div>
     </div>
