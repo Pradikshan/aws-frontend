22a6e32fca73f87544d575e7b9af4f59909f0850
Pradikshan <pradikshan234@gmail.com>
2024-08-24 11:10:21 +0530

    Added icon props

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -1,7 +1,7 @@
 import { BackgroundGradient } from "./ui/background-gradient";
 import { GiArtificialIntelligence } from "react-icons/gi";
 
-const FeaturedCard = ({ heading, text }) => {
+const FeaturedCard = ({ icon, heading, text }) => {
   return (
     <BackgroundGradient>
       <div className="bg-white rounded-[1.375rem] px-3 py-7">
@@ -11,12 +11,7 @@ const FeaturedCard = ({ heading, text }) => {
           </button> */}
           <div className="flex flex-row items-center">
             {/* <GiArtificialIntelligence className="text-3xl me-5" /> */}
-            <img
-              src="/public/images/music.png"
-              width={35}
-              alt="feature-icon"
-              className="me-4"
-            />
+            <img src={icon} width={35} alt="feature-icon" className="me-4" />
             <p className="text-lg font-bold">{heading}</p>
           </div>
           <p className="mt-3">{text}</p>
