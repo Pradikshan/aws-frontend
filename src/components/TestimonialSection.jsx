016499913c78e01f73a7d93fe8c00ce47507badc
Pradikshan <pradikshan234@gmail.com>
2024-08-24 12:09:24 +0530

    Changed section heading style

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TestimonialSection = () => {
-  return <div className="mt-36">TestimonialSection</div>;
+  return (
+    <div className="mt-36">
+      <p className="section-heading text-center">Testimonials</p>
+    </div>
+  );
 };
 
 export default TestimonialSection;
