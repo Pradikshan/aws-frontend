170e89b9071e0a8800820786902e6b6c09e61d23
Pradikshan <pradikshan234@gmail.com>
2024-08-24 21:05:02 +0530

    Created component

---
+++ src/components/Contactus.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contactus = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4 mx-8 mt-36 text-white">
+      <div>
+        <p className="text-4xl font-semibold">Get in touch!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contactus;
