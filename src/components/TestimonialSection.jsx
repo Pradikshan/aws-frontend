9d3f37b04dc3af8e27f7cc6cbb777994f8d9663e
Pradikshan <pradikshan234@gmail.com>
2024-08-24 12:02:55 +0530

    Added margin

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TestimonialSection = () => {
-  return <div>TestimonialSection</div>;
+  return <div className="mt-36">TestimonialSection</div>;
 };
 
 export default TestimonialSection;
