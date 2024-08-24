2e357eace9ecaefd0a9e80a59c62efaa7c8b6d21
Pradikshan <pradikshan234@gmail.com>
2024-08-24 10:57:49 +0530

    Removed unused code snippet

--- src/components/LampEffect.jsx
+++ src/components/LampEffect.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { LampContainer } from "./ui/lamp";
+import FeaturesOverview from "./FeaturesOverview";
 
 export function LampEffect() {
   return (
     <LampContainer>
+      {/* <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 py-4 text-center text-4xl font-medium tracking-tight text-white md:text-7xl"
+      >
+        Build lamps <br /> the right way
+      </motion.h1> */}
       <motion.h1
         initial={{ opacity: 0.5, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -13,9 +26,8 @@ export function LampEffect() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        Build lamps <br /> the right way
+        <FeaturesOverview />
       </motion.h1>
     </LampContainer>
   );
