7dca182f29de224404de634e0a564f490e5e7436
Pradikshan <pradikshan234@gmail.com>
2024-08-24 10:57:21 +0530

    Created component

---
+++ src/components/FeaturesBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FeaturesBox = () => {
+  return (
+    <div className="w-fit text-white p-3 rounded-lg border-2 border-white">
+      Hello
+    </div>
+  );
+};
+
+export default FeaturesBox;
