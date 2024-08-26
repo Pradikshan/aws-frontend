960e5e69aec08bb6347376fa3a93f07cc9fe28c4
Pradikshan <pradikshan234@gmail.com>
2024-08-26 17:06:44 +0530

    Added heading

--- src/views/Albums.jsx
+++ src/views/Albums.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { AlbumDetail } from "../components/AlbumDetail";
 
 const Albums = () => {
-  return <div>Albums</div>;
+  return (
+    <div className="mt-36">
+      <p className="text-white text-[2.8rem] font-semibold text-center">
+        View Albums
+      </p>
+      <AlbumDetail />
+    </div>
+  );
 };
 
 export default Albums;
