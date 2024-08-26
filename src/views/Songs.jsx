d103c6ae73a36b0d3529d6974c4ce967e5232714
Pradikshan <pradikshan234@gmail.com>
2024-08-26 19:31:05 +0530

    Created layout

--- src/views/Songs.jsx
+++ src/views/Songs.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Songs = () => {
-  return <div>Songs</div>;
+  return (
+    <div className="mt-36">
+      <p className="text-white text-[2.8rem] font-semibold text-center">
+        Songs Curated By DreamStreamer
+      </p>
+    </div>
+  );
 };
 
 export default Songs;
