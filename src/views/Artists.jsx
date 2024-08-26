ecf93306e2bce2ce05bff634619701ad8c4bf73d
Pradikshan <pradikshan234@gmail.com>
2024-08-26 11:39:01 +0530

    Changed margin top

--- src/views/Artists.jsx
+++ src/views/Artists.jsx
@@ -3,7 +3,10 @@ import { ArtistDetail } from "../components/ArtistDetail";
 
 const Artists = () => {
   return (
-    <div>
+    <div className="mt-36">
+      <p className="text-white text-[2.8rem] font-semibold text-center">
+        View Artists
+      </p>
       <ArtistDetail />
     </div>
   );
