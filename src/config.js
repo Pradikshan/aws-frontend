c2d75d9397f33158453f13583fc9b4414a007689
Pradikshan <pradikshan234@gmail.com>
2024-09-09 10:42:35 +0530

    Added user pool ID

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// src/config.js
+
+export const poolData = {
+  UserPoolId: "eu-west-1_M6MvynQRS", // Replace with your Cognito User Pool ID
+  ClientId: "6hep62fn6hsvjp1526vd9u1pd8", // Replace with your Cognito App Client ID
+};
