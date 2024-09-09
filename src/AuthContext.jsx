7f4f7113071b3524316c1adbb8e863613d974f95
Pradikshan <pradikshan234@gmail.com>
2024-09-09 11:52:32 +0530

    Added user attributes for user email

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -35,6 +35,16 @@ export const AuthProvider = ({ children }) => {
 
   const login = (user) => {
     setUser(user);
+    user.getUserAttributes((err, attributes) => {
+      if (err) {
+        console.error("Failed to get user attributes", err);
+      } else {
+        const emailAttr = attributes.find((attr) => attr.Name === "email");
+        if (emailAttr) {
+          setEmail(emailAttr.Value); // Set email
+        }
+      }
+    });
   };
 
   const logout = () => {
