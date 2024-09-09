82aa1691055d62bf25dcd60c2badc48b89596521
Pradikshan <pradikshan234@gmail.com>
2024-09-09 10:42:37 +0530

    Added user auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,20 @@
-"use client";
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
-import { Button } from "./ui/moving-border";
+import { AuthContext } from "../AuthContext";
 import { cn } from "../utils/cn.ts";
 
 export function Navbar() {
   return (
     <div className="relative w-full flex items-center justify-center">
       <NavbarFunc className="top-2" />
-      {/* <p className="text-black dark:text-white">
-        The Navbar will show on top of the page
-      </p> */}
     </div>
   );
 }
 
 function NavbarFunc({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
+  const { user } = useContext(AuthContext);
+
   return (
     <div
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
@@ -33,28 +31,30 @@ function NavbarFunc({ className }: { className?: string }) {
           </div>
         </MenuItem>
 
-        <MenuItem setActive={setActive} active={active} item="Music">
-          <div className="text-sm grid grid-cols-2 gap-10 p-4">
-            <ProductItem
-              title="View Albums"
-              href="/albums"
-              src="/images/album-cover.jpg"
-              description="All the albums curated by DreamStreamer"
-            />
-            <ProductItem
-              title="View Artists"
-              href="/artists"
-              src="/images/artists-cover.jpg"
-              description="All the Artists affiliated with DreamStreamer"
-            />
-            <ProductItem
-              title="View Songs"
-              href="/songs"
-              src="/images/song-cover.jpg"
-              description="All the songs curated by DreamStreamer"
-            />
-          </div>
-        </MenuItem>
+        {user && (
+          <MenuItem setActive={setActive} active={active} item="Music">
+            <div className="text-sm grid grid-cols-2 gap-10 p-4">
+              <ProductItem
+                title="View Albums"
+                href="/albums"
+                src="/images/album-cover.jpg"
+                description="All the albums curated by DreamStreamer"
+              />
+              <ProductItem
+                title="View Artists"
+                href="/artists"
+                src="/images/artists-cover.jpg"
+                description="All the Artists affiliated with DreamStreamer"
+              />
+              <ProductItem
+                title="View Songs"
+                href="/songs"
+                src="/images/song-cover.jpg"
+                description="All the songs curated by DreamStreamer"
+              />
+            </div>
+          </MenuItem>
+        )}
 
         <MenuItem setActive={setActive} active={active} item="Pricing">
           <div className="flex flex-col space-y-4 text-sm">
@@ -68,12 +68,6 @@ function NavbarFunc({ className }: { className?: string }) {
             <HoveredLink href="/support">Support</HoveredLink>
           </div>
         </MenuItem>
-        {/* <Button
-          borderRadius="1.75rem"
-          className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-        >
-          Borders are cool
-        </Button> */}
       </Menu>
     </div>
   );
