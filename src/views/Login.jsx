3d476ce75a692506ef146b6c202f1b5ed9b80046
Pradikshan <pradikshan234@gmail.com>
2024-09-15 15:38:14 +0530

    Made minor change

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -51,37 +51,45 @@ const Login = () => {
   };
 
   return (
-    <div className="mt-36 flex flex-col justify-center text-white">
-      <h2 className="text-white mx-auto font-bold text-2xl mb-10">Login</h2>
-      <form className="mx-auto" onSubmit={handleLogin}>
-        <div className="form-group">
-          <label className="text-white me-10">Username:</label>
-          <input
-            type="text"
-            className="form-control"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label className="text-white me-10">Password:</label>
-          <input
-            type="password"
-            className="form-control"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="p-3 font-semibold bg-gradient-to-r from-pink-500 to-violet-600 hover:bg-gradient-to-r hover:from-indigo-500 hover:to-purple-500 rounded-xl hover:scale-110 transition-all ease-in-out duration-500 text-white"
-        >
-          Login
-        </button>
-      </form>
-      {message && <div className="alert alert-info mt-3">{message}</div>}
+    // <div className="mt-36 flex flex-col justify-center ">
+    <div className="fixed inset-0 bg-black flex items-center justify-center">
+      <div className="text-center bg-gradient-to-r from-slate-500 to-slate-800 rounded-xl p-20">
+        <h2 className="text-white font-bold text-2xl mb-10">Login</h2>
+        <form className="mx-auto" onSubmit={handleLogin}>
+          <div className="form-group mb-5">
+            <label className="text-white me-10">Username:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group mb-5">
+            <label className="text-white me-10">Password:</label>
+            <input
+              type="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="p-3 font-semibold bg-gradient-to-r from-pink-500 to-violet-600 hover:bg-gradient-to-r hover:from-indigo-500 hover:to-purple-500 rounded-xl hover:scale-110 transition-all ease-in-out duration-500 text-white"
+          >
+            Login
+          </button>
+        </form>
+        <a href="/signup">
+          <p className="text-center underline text-decoration-sky-500 mt-5 text-sky-400">
+            Not signed up to DreamStreamer yet? Click here to Sign Up
+          </p>
+        </a>
+        {message && <div className="alert alert-info mt-3">{message}</div>}
+      </div>
     </div>
   );
 };
