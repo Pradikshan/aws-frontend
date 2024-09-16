1296d76600f51a9eebce7e0e7919edf5c591122c
Pradikshan <pradikshan234@gmail.com>
2024-09-16 19:26:31 +0530

    Added loading state

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,17 +1,19 @@
 // src/components/ProtectedRoute.js
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../AuthContext"; // Assuming you have AuthContext setup
+import { AuthContext } from "../AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useContext(AuthContext); // Check user authentication status
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <div>Loading...</div>; // Show a loading spinner or screen
+  }
 
   if (!user) {
-    // If the user is not logged in, redirect to the login page
     return <Navigate to="/login" replace />;
   }
 
-  // If user is logged in, render the protected component
   return children;
 };
 
