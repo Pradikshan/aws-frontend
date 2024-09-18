19e3ad52b1352b93c9b2939db28201a5835d2c30
Pradikshan <pradikshan234@gmail.com>
2024-09-18 09:05:32 +0530

    Made minor changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,17 @@
 // src/config.js
 
+// export const poolData = {
+//   UserPoolId: "eu-west-1_M6MvynQRS", // Replace with your Cognito User Pool ID
+//   ClientId: "6hep62fn6hsvjp1526vd9u1pd8", // Replace with your Cognito App Client ID
+// };
+
+// export const poolData = {
+//   UserPoolId: "eu-west-1_pKISgVYlu", // Replace with your Cognito User Pool ID
+//   ClientId: "309jschpfuijq374nr5p9icug8",
+//   ClientSecret: "1lja75bliju4lhl7qkk2o8nm9n7m9vf7qtdlgoqn6e5fve4ga6e1",
+// };
+
 export const poolData = {
-  UserPoolId: "eu-west-1_M6MvynQRS", // Replace with your Cognito User Pool ID
-  ClientId: "6hep62fn6hsvjp1526vd9u1pd8", // Replace with your Cognito App Client ID
+  UserPoolId: "eu-west-1_M6MvynQRS",
+  ClientId: "6hep62fn6hsvjp1526vd9u1pd8",
 };
