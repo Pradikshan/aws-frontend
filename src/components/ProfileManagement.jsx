d47e8dab4765d3b72959875a9af77c7f84eeecc5
Pradikshan <pradikshan234@gmail.com>
2024-09-18 12:42:53 +0530

    Created new component

---
+++ src/components/ProfileManagement.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileManagement = () => {
+  return (
+    <div className="border-2 border-white p-36 mt-36">
+      <p className="text-white text-4xl">Profile Management</p>
+    </div>
+  );
+};
+
+export default ProfileManagement;
