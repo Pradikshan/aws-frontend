bce35417cd7bf19c5da7985df357705fad87cfa0
Pradikshan <pradikshan234@gmail.com>
2024-09-18 12:43:05 +0530

    Created new view

---
+++ src/views/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileManagement from "../components/ProfileManagement";
+
+const Profile = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <ProfileManagement />
+    </div>
+  );
+};
+
+export default Profile;
