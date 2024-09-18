b45279bb503b9035c3a0b4f6f7e5579623847cde
Pradikshan <pradikshan234@gmail.com>
2024-09-18 12:42:36 +0530

    Changed credentials

--- src/config.js
+++ src/config.js
@@ -11,7 +11,13 @@
 //   ClientSecret: "1lja75bliju4lhl7qkk2o8nm9n7m9vf7qtdlgoqn6e5fve4ga6e1",
 // };
 
+//working userpool  data
 export const poolData = {
   UserPoolId: "eu-west-1_M6MvynQRS",
   ClientId: "6hep62fn6hsvjp1526vd9u1pd8",
 };
+
+// export const poolData = {
+//   UserPoolId: "eu-west-1_r40bgQVld",
+//   ClientId: "1vk6k9m2vk4mtopecfkha5r9sg",
+// };
