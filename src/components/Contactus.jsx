0dc695c34e61c6104485761baed02b397f1d1bab
Pradikshan <pradikshan234@gmail.com>
2024-10-07 23:58:54 +0530

    Made layout responsive

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -20,7 +20,7 @@ const Contactus = () => {
     },
   ];
   return (
-    <div className="grid grid-cols-2 gap-4 mx-8 mt-36 text-white">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-8 mt-36 text-white">
       <div>
         <p className="text-4xl font-semibold">Get in touch!</p>
       </div>
