4d5e488ed425c150b8fc0a30a2f6475e820c457d
Pradikshan <pradikshan234@gmail.com>
2024-10-07 23:59:03 +0530

    Made layout responsive

--- src/components/FeatureContainer.jsx
+++ src/components/FeatureContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FeatureContainer = ({ child1, child2 }) => {
   return (
     <div className="my-36">
-      <div className="grid grid-cols-2 gap-2 mx-8 text-white">
+      <div className="flex flex-col md:grid grid-cols-2 gap-2 mx-8 text-white">
         {child1}
         {child2}
       </div>
